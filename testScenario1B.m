32490ee6ffed4768c31b20f3d7c1a5c77fdca20d
fmenol <filippo.menolascina@ed.ac.uk>
2016-11-22 13:17:16 +0000

    Simulation file for Scenario 1B
    In this simulation I changed v2 to 0.001 and shifted the plotting time
    (just to cut the transient in the visualisation).

---
+++ testScenario1B.m
@@ -0,0 +1,17 @@
+clear;clc;
+load K
+load y0glu
+
+y0=repmat(10^-7,[1 5]);
+tt=linspace(0,3000,20)
+K(18)=1.814;
+K(23)=0.0477;
+K(7)=0.001;
+K(32)=4;
+K(36)=4;
+sol = dde23(@(t,sol,Z) IRMA(t,sol,Z,K,1,0,t), [100] ,y0,[0 3000]);
+
+out=[sol.x;sol.y]';
+plot(out(:,1),out(:,2:6));
+axis([400 1400 0 max(max(out(:,2:6)))*1.1])
+legend('CBF1','GAL4','SWI5','GAL80','ASH1');
